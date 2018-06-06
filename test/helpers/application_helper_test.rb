3f1df87015a9e98a13e9eff69282f0a77e6182c2
keisuke.ohashi <kog71903@gamil.com>
2018-06-06 12:21:10 +0000

    Make a basic User model (including secure passwords)

--- test/helpers/application_helper_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class ApplicationHelperTest < Action:View::TestCase
-    test "full title helper" do
-        asset_equal full_title,  FILL_IN
-        asset_equal full_title("Help"), FILL_IN
-    end
-    
-end
